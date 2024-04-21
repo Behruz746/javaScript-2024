3f17ebecab3860cf4a09542c3a63538dd6af6ee1
Behruz <madaminovbehruz746@gmail.com>
2024-04-21 23:15:05 +0500

    added: DOM methods

--- 11-dars-DOM-bilan-ishlash/js/script.js
+++ 11-dars-DOM-bilan-ishlash/js/script.js
@@ -2,19 +2,42 @@
 
 const boxEL = document.querySelector(".box");
 const circleEL = document.querySelectorAll(".circle");
-const btnEl = document.getElementsByTagName("button");
+const btnEl = document.querySelectorAll("button");
+const heartEl = document.querySelectorAll(".heart");
 
-for (let i = 0; i < btnEl.length; i++) {
-  btnEl[i].textContent = `index: ${i}`;
-  console.log(btnEl[i].textContent);
+// New elements
+let btn = document.createElement("button");
+let myCircleEl = document.createElement("div");
+myCircleEl.classList.add("circle");
+myCircleEl.textContent = "F";
 
-  btnEl[i].addEventListener(
-    "click",
-    (e) => (e.target.textContent = `index: ${i++}`)
-  );
-}
+console.dir(boxEL);
 
-circleEL.forEach((item) => {
-  item.style.backgroundColor = "#000";
-  console.log(item);
+boxEL.style.cssText = `background-color: red; 
+  border-radius: 20px;`;
+
+heartEl.forEach((item) => {
+  item.style.cssText = `background-color: green;`;
 });
+
+document.querySelector(".circle__wrapper").append(myCircleEl);
+circleEL[1].replaceWith(myCircleEl);
+
+document
+  .querySelector(".circle__wrapper")
+  .insertAdjacentHTML("beforeend", `<h1>1</h1>`);
+
+// for (let i = 0; i < btnEl.length; i++) {
+//   btnEl[i].textContent = `index: ${i}`;
+//   console.log(btnEl[i].textContent);
+
+//   btnEl[i].addEventListener(
+//     "click",
+//     (e) => (e.target.textContent = `count: ${i++}`)
+//   );
+// }
+
+// circleEL.forEach((item) => {
+//   item.style.backgroundColor = "#000";
+//   console.log(item);
+// });
