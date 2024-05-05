55a0bbdfc8531d3be9bee3debad537594cde2590
Behruz <madaminovbehruz746@gmail.com>
2024-05-05 15:50:57 +0500

    added: lesson for Function Inkapsulatsion constroctor class

--- 33-dars-inkapsulatsiya-constructor-class/script.js
+++ 33-dars-inkapsulatsiya-constructor-class/script.js
@@ -25,8 +25,42 @@ function Car(name, color, bonus) {
 }
 
 const bmw = new Car("bmw", "#000", 10);
-console.log(bmw.getBonus());
-bmw.setBonus(20);
-bmw.info();
+// console.log(bmw.getBonus());
+// bmw.setBonus(20);
+// bmw.info();
 
 // Class Inkapsulatsion constroctor
+class Cars {
+  constructor(name, color, bonus) {
+    this.name = name;
+    this.color = color;
+    this._extrapBonus = bonus; // _ Ladash
+  }
+
+  // Info function
+  info() {
+    console.log(
+      `Name of car ${this.name}, color is ${this.color}, there is some bonus ${this._extrapBonus}`
+    );
+  }
+
+  // getBonus function
+  get bonus() {
+    return this._extrapBonus;
+  }
+
+  // setBonus function
+  set bonus(bonus) {
+    if (typeof bonus === "number" && bonus > 0 && bonus < 100) {
+      this._extrapBonus = bonus;
+    } else {
+      console.log("Something went wrong");
+    }
+  }
+}
+
+const merc = new Cars("MERC", "#fff", 10);
+console.log(merc._extrapBonus);
+console.log(merc.bonus);
+merc.bonus = 50;
+merc.info();
